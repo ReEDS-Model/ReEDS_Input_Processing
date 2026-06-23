fead5c22493fb5e58e70eb23768955e781d7cdc1
jcarag <Vincent.Carag@nrel.gov>
2026-06-23 13:35:17 -0600

    Wrap script logic in main() function

--- psh/calculate_existing_psh_capacities.py
+++ psh/calculate_existing_psh_capacities.py
@@ -14,7 +14,7 @@ Procedures are adapted from the original version of this script developed by @ko
 @author: jvcarag
 @date: 20260618 12:21
 """
-#%%
+
 import pandas as pd
 import os
 import geopandas as gpd
@@ -24,76 +24,80 @@ reeds_path = os.path.expanduser('~/github/ReEDS')
 sys.path.append(reeds_path)
 import reeds
 
-#%% Paths
-outdir_path = os.path.join(reeds_path, 'inputs', 'storage')
+def main():
+    outdir_path = os.path.join(reeds_path, 'inputs', 'storage')
 
-# Ingest county-level CONUS shapefile 
-dfcounty = reeds.spatial.get_map('county').reset_index()
-# Ingest operational data for existing PSH fleet
-psh_data_in = pd.read_excel(
-    os.path.join('data','GESDB_Projects_complete RS_v3_fromORNL.xlsx'),
-    sheet_name='Summary'
-)
-
-#%% Convert site-level PSH data to geopandas dataframe
-psh_data = (
-    gpd.GeoDataFrame(
-        psh_data_in,
-        geometry=[
-            Point(xy)
-            for xy
-            in zip(psh_data_in['Longitude'], psh_data_in['Latitude'])
-        ]
+    # Ingest county-level CONUS shapefile 
+    dfcounty = reeds.spatial.get_map('county').reset_index()
+    # Ingest operational data for existing PSH fleet
+    psh_data_in = pd.read_excel(
+        os.path.join('data','GESDB_Projects_complete RS_v3_fromORNL.xlsx'),
+        sheet_name='Summary'
     )
-    .set_crs(epsg=4326)
-)
-# Spatially join with counties to determine each plant's county
-psh_data = (
-    psh_data.to_crs(dfcounty.crs)
-    .sjoin(dfcounty[['GEOID', 'geometry']])
-    .rename(columns={'GEOID': 'r'})
-    .drop(
-        columns=[
-            'Latitude',
-            'Longitude',
-            'geometry',
-            'index_right'
-        ]
+
+    # Convert site-level PSH data to geopandas dataframe
+    psh_data = (
+        gpd.GeoDataFrame(
+            psh_data_in,
+            geometry=[
+                Point(xy)
+                for xy
+                in zip(psh_data_in['Longitude'], psh_data_in['Latitude'])
+            ]
+        )
+        .set_crs(epsg=4326)
     )
-)
-# Add 'p' prefix to all FIPS in 'r' column
-psh_data['r'] = 'p' + psh_data['r']
+    # Spatially join with counties to determine each plant's county
+    psh_data = (
+        psh_data.to_crs(dfcounty.crs)
+        .sjoin(dfcounty[['GEOID', 'geometry']])
+        .rename(columns={'GEOID': 'r'})
+        .drop(
+            columns=[
+                'Latitude',
+                'Longitude',
+                'geometry',
+                'index_right'
+            ]
+        )
+    )
+    # Add 'p' prefix to all FIPS in 'r' column
+    psh_data['r'] = 'p' + psh_data['r']
 
-# Add tech and tech vintage columns (needed later in ReEDS), 
-psh_data.insert(0, '*i', 'pumped-hydro')
-psh_data.insert(0, 'v', 'init-1')
-# Convert Rated Power to MW
-psh_data['Rated Power (MW)'] = psh_data['Rated Power(kW)'] * 1e-3
-# Create a duplicate column as a placeholder for pump capacity 
-psh_data['Pump Capacity (MW)'] = psh_data['Rated Power (MW)'].copy()
+    # Add tech and tech vintage columns (needed later in ReEDS), 
+    psh_data.insert(0, '*i', 'pumped-hydro')
+    psh_data.insert(0, 'v', 'init-1')
+    # Convert Rated Power to MW
+    psh_data['Rated Power (MW)'] = psh_data['Rated Power(kW)'] * 1e-3
+    # Create a duplicate column as a placeholder for pump capacity 
+    psh_data['Pump Capacity (MW)'] = psh_data['Rated Power (MW)'].copy()
 
-# Calculate county-level operational capacity, pump capacity, and max energy
-psh_data_out = (
-        psh_data.rename(columns={
-        'Title': 'station',
-        'Rated Power (MW)': 'operational_capacity_MW',
-        'Pump Capacity (MW)': 'pump_capacity_MW',
-        'Energy (MWh)': 'max_energy_MWh'
-    })
-    [[
-        '*i',
-        'v',
-        'r',
-        'station',
-        'operational_capacity_MW',
-        'pump_capacity_MW',
-        'max_energy_MWh'
-    ]]
-    .groupby(['*i', 'v', 'r'])
-    .sum(numeric_only=True)
-    .round(1)
-)
-#%% Output data to ReEDS inputs folder
-psh_data_out.to_csv(os.path.join(outdir_path, 'cap_existing_psh.csv'))
+    # Calculate county-level operational capacity, pump capacity, and max energy
+    psh_data_out = (
+            psh_data.rename(columns={
+            'Title': 'station',
+            'Rated Power (MW)': 'operational_capacity_MW',
+            'Pump Capacity (MW)': 'pump_capacity_MW',
+            'Energy (MWh)': 'max_energy_MWh'
+        })
+        [[
+            '*i',
+            'v',
+            'r',
+            'station',
+            'operational_capacity_MW',
+            'pump_capacity_MW',
+            'max_energy_MWh'
+        ]]
+        .groupby(['*i', 'v', 'r'])
+        .sum(numeric_only=True)
+        .round(1)
+    )
+    # Output data to ReEDS inputs folder
+    psh_data_out.to_csv(os.path.join(outdir_path, 'cap_existing_psh.csv'))
 
-print(f"Run complete. See {outdir_path} for outputs.")
+    print(f"Run complete. See {outdir_path} for outputs.")
+
+
+if __name__ == "__main__":
+    main()
